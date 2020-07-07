904de11980483b3712bc1ff7a6bc8fd50be4384e
Igor <igor@look.pet>
2020-07-08 00:24:41 +0300

    facebook test

--- src/Controller/FaceBookController.php
+++ src/Controller/FaceBookController.php
@@ -36,6 +36,10 @@ class FaceBookController extends AbstractController
      */
     public function facebook(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
+        return new JsonResponse([
+            'Fuck Svetoslav'
+        ], Response::HTTP_FORBIDDEN);
+
         if ($request->request->has('userID')) {
             $user = null;
 
