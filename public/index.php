69c11a85ea7d82bd1492319aec6cd3973815c332
Igor <i.sinitsyn@look.pet>
2020-07-31 22:57:58 +0300

    ebl

--- public/index.php
+++ public/index.php
@@ -12,10 +12,9 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-Request::setTrustedProxies(
-    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
-    Request::HEADER_X_FORWARDED_AWS_ELB
-);
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
