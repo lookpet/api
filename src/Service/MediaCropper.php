70805de42742114e048d084223e3930fdc2df874
Igor <i.sinitsyn@look.pet>
2020-08-10 18:17:32 +0300

    crop

--- src/Service/MediaCropper.php
+++ src/Service/MediaCropper.php
@@ -59,7 +59,6 @@ class MediaCropper implements MediaCropperInterface
 
         $tmpFile = '/tmp/' . Uuid::uuid4()->toString() . '.jpg';
         file_put_contents($tmpFile, $file);
-        sleep(2);
 
         $resizer = new ImageResize(
             $tmpFile
@@ -69,6 +68,8 @@ class MediaCropper implements MediaCropperInterface
         $resizer->save(
             $filePath
         );
+
+        sleep(5);
 
         $stream = fopen($filePath, 'rb');
         $this->filesystem->write(
