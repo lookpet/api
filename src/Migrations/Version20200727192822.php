472047bdf56ae1ba6a159554c6a7f8f6ad4f9967
Igor <i.sinitsyn@look.pet>
2020-11-06 22:30:42 +0300

    migration rebuild

--- src/Migrations/Version20200727192822.php
+++ src/Migrations/Version20200727192822.php
@@ -17,7 +17,7 @@ final class Version20200727192822 extends AbstractMigration
         return 'Lets Pet! 27 July 2020!';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -56,7 +56,7 @@ final class Version20200727192822 extends AbstractMigration
         $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
