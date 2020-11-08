fa3ac052bab0c783a3f5540018ecc3d9da2ade6b
Igor <i.sinitsyn@look.pet>
2020-11-08 03:00:34 +0300

    is_deleted default

--- src/Migrations/Version20201107235646.php
+++ src/Migrations/Version20201107235646.php
@@ -12,25 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201107235646 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
-        return '';
+        return 'pet is deleted false default';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('ALTER TABLE pet CHANGE is_deleted is_deleted TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE pet CHANGE is_deleted is_deleted TINYINT(1) NOT NULL');
     }
 }
